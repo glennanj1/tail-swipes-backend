6bd11b64728a3ec70d71928a27bad92527049823
github username <github email address>
2021-05-16 07:12:08 -0400

    seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do 
+    Profile.create([{ name: Faker::Creature::Dog.name, match: true, image: 'https://placedog.net/640/480?random'}])
+end
