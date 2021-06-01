d3e171120883de3c134d49754e63e5ebebd0c6b7
john glennan <glennan20@gmail.com>
2021-06-01 19:37:46 -0400

    nested convos under messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :messages
+  
+  resources :messages do 
+    resources :conversations
+  end
   resources :profiles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
